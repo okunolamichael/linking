3f962f3112098ab32627f588d905c99bb673099a
okunolamichael <59531059+okunolamichael@users.noreply.github.com>
2020-05-22 06:00:32 -0700

    Add files via upload

---
+++ linking/firstpage/first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first page</title>
+
+    <link rel="stylesheet" href="linking/second">
+</head>
+<body>
+    
+    <a href="../linking/secondpage/second.php">click</a>
+</body>
+</html>
